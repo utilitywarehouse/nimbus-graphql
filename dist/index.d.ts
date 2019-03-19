08fac01e76ed11a5587f3102177af4b67b10ec2a
fenos <fabri.feno@gmail.com>
2019-03-19 16:03:37 +0000

    Importing submodules

---
+++ dist/index.d.ts
@@ -0,0 +1,9 @@
+import './config';
+import './errors';
+import './framework';
+import './gql';
+import './grpc';
+import './http';
+import './logger';
+import './operational';
+import './repository';
---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./config");
+require("./errors");
+require("./framework");
+require("./gql");
+require("./grpc");
+require("./http");
+require("./logger");
+require("./operational");
+require("./repository");
+//# sourceMappingURL=index.js.map
