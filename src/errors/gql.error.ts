36fa756b3a44d4fc2e598e77c03eb1a7a80ec963
Michael Donat <mdonat@utilitywarehouse.co.uk>
2019-05-12 13:54:38 +0100

    Update gql.error.ts

--- src/errors/gql.error.ts
+++ src/errors/gql.error.ts
@@ -33,9 +33,12 @@ export class GQLError extends BaseError {
         details.path = this.gql.path;
         details.extensions = this.gql.extensions;
         
+        details.extensions.exception.type = this.type
+        details.extensions.exception.message = this.message
+
         if (this.previous && this.previous.render) {
             details.extensions.exception.previous = this.previous.render()
-        } else if (this.previous && this.previous.message)) {
+        } else if (this.previous && this.previous.message) {
             details.extensions.exception.previous = this.previous.message
         }
         
