fe5a9d0c13190ab1b46b02f249c55b3a909c9764
Gilmar Quinelato <gilmarsquinelato@gmail.com>
2019-09-11 13:49:43 +0100

    Remove http client exception throw based on status code

--- src/http/http.url.ts
+++ src/http/http.url.ts
@@ -1,26 +1,14 @@
 import * as templates from 'uri-template';
 
-interface Parameters {
-    [k: string]: string;
-}
-
 export class URL {
-  private constructor(private readonly template: string, private readonly parsed: string) {
+  private constructor(private readonly template: string, private readonly parsed: string) { }
 
-  }
-
-  static template(template: string, parameters?: Parameters): URL {
-
+  static template(template: string, parameters?: Record<string, string>) {
     const tpl = templates.parse(template);
-
     return new URL(template, tpl.expand(parameters));
   }
 
-  urlTemplate(): string {
-    return this.template;
-  }
+  urlTemplate = () => this.template;
 
-  toString(): string {
-    return this.parsed;
-  }
+  toString = () => this.parsed;
 }
--- src/http/index.ts
+++ src/http/index.ts
@@ -1,2 +1,3 @@
 export { createClient, HttpClient } from './http.client';
 export { URL } from './http.url';
+export { default as getErrorFromStatusCode } from './http.errors';
